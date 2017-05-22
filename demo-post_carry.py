f3fe49403a12ddd533511ddc436b3a73a0667532
Jose Miguel Delgado <jmg4delgado@gmail.com>
2017-05-22 22:27:39 +0200

    Update demo-post_carry.py

--- demo-post_carry.py
+++ demo-post_carry.py
@@ -1,36 +1,20 @@
 #!/usr/bin/python
-import http.client
-import urllib.request
-import urllib.parse
 import json
-import time
 
-api_url = "https://api.carriots.com/streams"
-device = "RPI1@jmg4carriots.jmg4carriots" # Replace with the "id_developer" of your device
-api_key = "eca83189ebe6f566b328949ad9fd47d857f31c82c197a190f9c9fd38464c9c66"
-
-# Get the time
-timestamp = int(time.time())
 
 #Parameters - Body (data)
+"""
 params = {"protocol":"v2",
               "device":"RPI1@jmg4carriots.jmg4carriots",
               "at":timestamp,
               "data": dict(
                   temp=27,
                   humd=71)}  
-
-binary_data = json.dumps(params).encode('ascii')
-
-#Header
-header = {"User-Agent": "raspberrycarriots",
-              "Content-Type": "application/json",
-              "carriots.apikey": api_key}
-
-#Request
-req = urllib.request.Request(api_url,binary_data,header)
-f = urllib.request.urlopen(req)
-
+"""                  
+hr = 60
+temp = 24
+l = 0.34
+f = carriots.postDataCarriots(hr,temp,l)
 
 #Print in a pretty way
 data=json.loads(f.read().decode('utf-8'))
